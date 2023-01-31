572c393a2b81ec46ecc2539a4d3d2170e1858d45
David M. Chen <chen7david@me.com>
2023-02-01 06:50:47 +0800

    added get ddb query

--- src/handler/get-all-users.ts
+++ src/handler/get-all-users.ts
@@ -5,17 +5,25 @@ const ddb = new DynamoDB.DocumentClient()
 export const handler = async () => {
   const tableName: string = process.env.TABLE_NAME || ''
 
-  const params = {
+  const putQuery = {
     TableName: tableName,
     Item: {
       email: 'chen7david',
-      friends: [1,2,3,4]
+      friends: [1, 2, 3, 4]
+    }
+  }
+
+  const getQuery = {
+    TableName: tableName,
+    Key: {
+      PartitionKey: 'chen7david'
     }
   }
 
   try {
-    await ddb.put(params).promise()
-    return { statusCode: 200, body: 'Item added successfully' }
+    await ddb.put(putQuery).promise()
+    const data = await ddb.get(getQuery)
+    return { statusCode: 200, body: JSON.stringify(data) }
   } catch (error) {
     return { statusCode: 500, body: error }
   }
