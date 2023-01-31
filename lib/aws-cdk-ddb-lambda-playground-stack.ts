070bc727bc4be5c274741791ffb5431fa7d88f3c
David M. Chen <chen7david@me.com>
2023-02-01 06:41:52 +0800

    updated params of ddb palyoad

--- lib/aws-cdk-ddb-lambda-playground-stack.ts
+++ lib/aws-cdk-ddb-lambda-playground-stack.ts
@@ -1,5 +1,9 @@
 import * as cdk from 'aws-cdk-lib'
-import { aws_dynamodb as dynamodb, aws_apigateway as apigw, aws_lambda as lambda } from 'aws-cdk-lib'
+import {
+  aws_dynamodb as dynamodb,
+  aws_apigateway as apigw,
+  aws_lambda as lambda
+} from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 // import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -9,28 +13,32 @@ export class AwsCdkDdbLambdaPlaygroundStack extends cdk.Stack {
     super(scope, id, props)
 
     const userTableSchema: dynamodb.TableProps = {
-      tableName: 'aws-cdk-lambda-playground-user-table',
+      tableName: 'aws-cdk-lambda-playground-user-db-table',
       partitionKey: {
         name: 'email',
         type: dynamodb.AttributeType.STRING
-      },
+      }
     }
 
     const userTable = new dynamodb.Table(
       this,
-      'aws-cdk-lambda-playground',
+      'aws-cdk-lambda-playground-user-db-table-id',
       userTableSchema
     )
 
-    const userLamnda =  new lambda.Function(this, 'aws-cdk-lambda-playground', {
-      functionName: 'aaws-cdk-lambda-playground-user',
-      code: new lambda.AssetCode('src/handler/'),
-      handler: 'get-all-users.handler',
-      runtime: lambda.Runtime.NODEJS_14_X,
-      environment: {
-        TABLE_NAME: userTable.tableName
+    const userLamnda = new lambda.Function(
+      this,
+      'aws-cdk-lambda-playground-labmda',
+      {
+        functionName: 'aaws-cdk-lambda-playground-user',
+        code: new lambda.AssetCode('src/handler/'),
+        handler: 'get-all-users.handler',
+        runtime: lambda.Runtime.NODEJS_14_X,
+        environment: {
+          TABLE_NAME: userTable.tableName
+        }
       }
-    })
+    )
 
     userTable.grantWriteData(userLamnda)
   }
--- src/handler/get-all-users.ts
+++ src/handler/get-all-users.ts
@@ -3,19 +3,20 @@ import { DynamoDB, Lambda, AWSError } from 'aws-sdk'
 const ddb = new DynamoDB.DocumentClient()
 
 export const handler = async () => {
-    const tableName: string = process.env.TABLE_NAME || ''
+  const tableName: string = process.env.TABLE_NAME || ''
 
-    const params = {
-        TableName: tableName,
-        Item: {
-            email: "chen7david"
-        }
+  const params = {
+    TableName: tableName,
+    Item: {
+      email: 'chen7david',
+      friends: [1,2,3,4]
     }
+  }
 
-    try {
-        await ddb.put(params).promise()
-        return { statusCode: 200, body: 'Item added successfully' };
-    } catch (error) {
-        return { statusCode: 500, body: error };
-    }
+  try {
+    await ddb.put(params).promise()
+    return { statusCode: 200, body: 'Item added successfully' }
+  } catch (error) {
+    return { statusCode: 500, body: error }
+  }
 }
